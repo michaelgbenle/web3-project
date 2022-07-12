a69435d3c343892ca66aed43dc5c99709dd3fb43
michaelgbenle <michaelgbenle@gmail.com>
2022-07-12 09:06:34 -0700

    project started

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	r := mux.newRouter()
 	r.HandleFunc("/", ).Methods("GET")
+	r.HandleFunc("/", ).Methods("POST")
+	r.HandleFunc("/", ).Methods("POST")
 	fmt.Println("connectring to server")
 
 }
