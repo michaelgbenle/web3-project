479ecfb797f59f0f30ade7f0cee81f0a400cf217
michaelgbenle <michaelgbenle@gmail.com>
2022-07-12 10:38:07 -0700

    project started

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ if ValidBlock(block, prevBlock){
 }
 func CreateBlock(prevBlock *Block, checkoutItem BookCheckout) *Block{
 block := &Block{}
-block.Position =
+block.Position = prevBlock.Position + 1
 }
 
 
